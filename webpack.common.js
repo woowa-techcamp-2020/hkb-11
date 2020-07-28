dacac98ce0a839f431c8e8d8ac91db7959fa9c1d
agrajak <korbots@gmail.com>
2020-07-28 10:51:23 +0900

    [#1] setting(project) - 프로젝트 폴더 설정

---
+++ webpack.common.js
@@ -0,0 +1,86 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports = {
+  entry: './frontend/index.js',
+  resolve: {
+    alias: {
+      api: './frontend/api',
+      components: path.resolve(__dirname, 'frontend/components'),
+      styles: path.resolve(__dirname, 'frontend/styles'),
+      utils: path.resolve(__dirname, 'frontend/utils'),
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, 'backend/public/dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+                ["@babel/preset-env", {
+                    corejs: 3,
+                    targets: '> 1%',
+                    useBuiltIns: "usage"
+                }]
+            ],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          }
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: 'images/[name].[ext]?[hash]',
+              limit: 10000,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      cache: false,
+      template: 'frontend/static/index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 6,
+      maxInitialRequests: 4,
+      automaticNameDelimiter: '~',
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+}
---
+++ webpack.dev.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const common = require('./webpack.common.js')
+const { merge } = require('webpack-merge')
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, "./frontend/static"),
+    port: 9000,
+    proxy: {
+      '/api/': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true,
+      }
+    }
+  }
+})
