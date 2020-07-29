a42855b1a1e7018b5167dcaed717f127d7eedcbf
Choibumsu <bumsu0211@gmail.com>
2020-07-30 01:15:07 +0900

    [#13] feat(payment method) - 결제수단 삭제 컨트롤러 작성
    - 삭제할 결제수단이 존재하지 않으면 404
    - 존재하는데 delete 쿼리 이후에 변경된 사항이 없다면 500
    - 정상적으로 삭제되면 200 리턴

--- backend/controller/payment-method.ts
+++ backend/controller/payment-method.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express'
-import { OkPacket } from 'mysql2'
+import { OkPacket, ResultSetHeader, RowDataPacket } from 'mysql2'
 import pool from '../pool'
 import query from '../query'
 
 const getPaymentMethodList = async (req: Request, res: Response) => {
   try {
+    // jwt token 검사, false: 401
+
     const [rows] = await pool.query(query.SELECT_PAYMENT_METHOD_LIST)
     res.json({
       paymentMethodList: rows,
@@ -39,7 +41,45 @@ const postPaymentMethod = async (req: Request, res: Response) => {
   }
 }
 
+const deletePaymentMethod = async (req: Request, res: Response) => {
+  try {
+    // jwt token 검사, false: 401
+
+    // TODO : req.body에 id만 보낼지 paymentMethod로 감싸서 보낼지 논의
+    const {
+      paymentMethod: { id },
+    } = req.body
+
+    // 삭제할 paymentMethod가 있는지 검사, false: 404
+    const [row] = await pool.query<RowDataPacket[]>(
+      query.SELECT_PAYMENT_METHOD,
+      [id]
+    )
+    if (row.length === 0) {
+      res.status(404).json()
+      return
+    }
+
+    const [result] = await pool.query<ResultSetHeader>(
+      query.DELETE_PAYMENT_METHOD,
+      [id]
+    )
+
+    // 변경된 사항이 없다면(삭제된 것이 없다면) Error를 발생시켜 500 리턴
+    if (result.affectedRows === 0) {
+      throw Error
+    }
+
+    res.status(200).json()
+  } catch (e) {
+    res.status(500).json({
+      message: e,
+    })
+  }
+}
+
 export default {
   getPaymentMethodList,
   postPaymentMethod,
+  deletePaymentMethod,
 }
