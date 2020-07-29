c1877e250269021a8a081f99207b2d25ae53b953
Choibumsu <bumsu0211@gmail.com>
2020-07-30 00:26:38 +0900

    [#13] feat(payment method) - 결제수단 추가 API 작성
    - insert 쿼리 작성
    - userId는 임시 아이디를 사용

--- backend/controller/payment-method.ts
+++ backend/controller/payment-method.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { OkPacket } from 'mysql2'
 import pool from '../pool'
 import query from '../query'
 
@@ -15,6 +16,30 @@ const getPaymentMethodList = async (req: Request, res: Response) => {
   }
 }
 
+const postPaymentMethod = async (req: Request, res: Response) => {
+  try {
+    // jwt token 검사
+    const userId = 'agrajak2' //temp userId
+    const {
+      paymentMethod: { title },
+    } = req.body
+
+    const [row] = await pool.query<OkPacket>(query.INSERT_PAYMENT_METHOD, [
+      title,
+      userId,
+    ])
+
+    res.json({
+      paymentMethodId: row.insertId,
+    })
+  } catch (e) {
+    res.status(500).json({
+      message: e,
+    })
+  }
+}
+
 export default {
   getPaymentMethodList,
+  postPaymentMethod,
 }
--- backend/query.ts
+++ backend/query.ts
@@ -3,5 +3,6 @@ const queries = {
     'INSERT INTO Invoice (date, categoryId, paymentMethodId, item, amount) VALUES (?, ?, ?, ?, ?)',
   SELECT_CATEGORY_LIST: `SELECT * FROM Category`,
   SELECT_PAYMENT_METHOD_LIST: `SELECT * FROM PaymentMethod`,
+  INSERT_PAYMENT_METHOD: `INSERT INTO PaymentMethod (title, userId) VALUES (?, ?)`,
 }
 export default queries
