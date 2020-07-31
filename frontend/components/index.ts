95285cbe0fe437745cc6d70dfc74a3317a73e073
agrajak <korbots@gmail.com>
2020-07-31 13:49:00 +0900

    [#28] fix(component) - 부모 컴포넌트의 타입도 제네릭으로 추론하도록 설정

--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -1,10 +1,10 @@
 import { View } from '../view'
 
-export class Component<T extends View> {
+export class Component<T extends View, G extends Component<View, any> = any> {
   view: T
-  parent
+  parent: G
   constructor(parent, view: T) {
-    this.parent = parent as Component<View>
+    this.parent = parent as G
     this.view = view
   }
   mount(element: HTMLElement) {
