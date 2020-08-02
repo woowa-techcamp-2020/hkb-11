215c4178b1c60d7927a75d9d4b725e110bab4cfe
Choibumsu <bumsu0211@gmail.com>
2020-08-02 17:03:54 +0900

    [#32] style(form) - FormView 스타일 작업
    - 스타일 1차 작업
    - 스타일 작업으로 인해 마크업 일부 수정
    - button type을 버튼으로 수정
    - select placeholder, input date placeholder 수정 필요

--- frontend/view/ContainerView/RouterView/FormView/template.ts
+++ frontend/view/ContainerView/RouterView/FormView/template.ts
@@ -2,27 +2,30 @@ export const template: string = `
   <section id='invoice-form'>
     <form>
       <div class="float">
-        <button class="button-clear-form">내용 지우기</button>
-        <button class="button-remove-invoice hidden">삭제</button>
-      </div>
-      <div class="row">
-        <label>분류</label>
-        <button class="button earning-toggle active">수입</button>
-        <button class="button spending-toggle">지출</button>
+        <button type='button' class="button-clear-form">내용 지우기</button>
+        <button type='button' class="button-remove-invoice hidden">삭제</button>
       </div>
       <div class="row">
         <div class="item">
-          <label>날짜</label>
+          <label class="form-label">분류</label>
+          <button type='button' class="button earning-toggle active">수입</button>
+          <button type='button' class="button spending-toggle">지출</button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="item">
+          <label class="form-label">날짜</label>
           <input type="date" class="input-date" />
         </div>
         <div class="item">
-          <label>카테고리</label>
-          <select class="select-category">
-            <option value="">선택하세요</option>
+          <label class="form-label">카테고리</label>
+          <select class="select-category" value=''>
+            <option value="" disabled selected>선택하세요</option>
+            <option value='123'>123</option>
           </select>
         </div>
         <div class="item">
-          <label>결제수단</label>
+          <label class="form-label">결제수단</label>
           <select class="select-payment">
             <option value="">선택하세요</option>
           </select>
@@ -30,12 +33,12 @@ export const template: string = `
       </div>
       <div class="row">
         <div class="item">
-          <label>금액</label>
-          <input class="input-amount" />
+          <label class="form-label">금액</label>
+          <input class="input-amount" placeholder='금액을 입력해주세요' />
         </div>
         <div class="item">
-          <label>내용</label>
-          <input class="input-item" />
+          <label class="form-label">내용</label>
+          <input class="input-item" placeholder='내용을 입력해주세요' />
         </div>
       </div>
       <div class="row">
