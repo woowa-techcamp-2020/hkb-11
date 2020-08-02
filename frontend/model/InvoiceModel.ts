fb8de86f47a51d4b2af5d7fa3dea41d53b1481aa
Choibumsu <bumsu0211@gmail.com>
2020-08-02 22:53:01 +0900

    [#32] feat(invoice model) - id로 invoice를 반환해주는 함수 작성
    - getInvoice(id) : id를 인자로 받고, 이에 해당하는 invoice 객체를 반환

--- frontend/model/InvoiceModel.ts
+++ frontend/model/InvoiceModel.ts
@@ -1,6 +1,6 @@
 import { Observable } from '.'
 import { Invoice } from '../../types'
-import { EVENTS } from '../utils/constants'
+import { EVENT } from '../utils/constants'
 
 export class InvoiceModel extends Observable {
   invoices: Array<Invoice> = []
@@ -10,18 +10,22 @@ export class InvoiceModel extends Observable {
   earningToggle: boolean = true
   spendingToggle: boolean = true
 
+  getInvoice(id: number): Invoice | undefined {
+    return this.invoices.find((invoice) => invoice.id === id)
+  }
+
   addSumEarning(offset: number) {
     this.sumEarning += offset
-    this.emit(EVENTS.SET_SUM_EARNING, this.sumEarning)
+    this.emit(EVENT.SET_SUM_EARNING, this.sumEarning)
   }
   addSumSpending(offset: number) {
     this.sumSpending += offset
-    this.emit(EVENTS.SET_SUM_SPENDING, this.sumSpending)
+    this.emit(EVENT.SET_SUM_SPENDING, this.sumSpending)
   }
   addInvoice(invoice: Invoice) {
     this.invoices = [...this.invoices, invoice]
     const { category, amount } = invoice
-    this.emit(EVENTS.ADD_INVOICE, invoice)
+    this.emit(EVENT.ADD_INVOICE, invoice)
     if (category.type === '수입') {
       this.addSumEarning(amount)
       return
@@ -30,11 +34,11 @@ export class InvoiceModel extends Observable {
   }
   setEarningToggle(value) {
     this.earningToggle = value
-    this.emit(EVENTS.EARNING_TOGGLE, value)
+    this.emit(EVENT.EARNING_TOGGLE, value)
   }
   setSpendingToggle(value) {
     this.spendingToggle = value
-    this.emit(EVENTS.SPENDING_TOGGLE, value)
+    this.emit(EVENT.SPENDING_TOGGLE, value)
   }
   removeInvoice(id) {
     const invoice = this.invoices.find((x) => x.id === id)
@@ -43,7 +47,7 @@ export class InvoiceModel extends Observable {
     if (category.type === '수입') this.addSumEarning(-amount)
     else this.addSumSpending(-amount)
     this.invoices = this.invoices.filter((x) => x !== invoice)
-    this.emit(EVENTS.REMOVE_INVOICE, id)
+    this.emit(EVENT.REMOVE_INVOICE, id)
   }
   setInvoices(invoices: Array<Invoice>) {
     this.clear()
@@ -55,23 +59,23 @@ export class InvoiceModel extends Observable {
   updateInvoice(invoice: Invoice) {
     const { id } = invoice
     this.removeInvoice(id)
-    this.emit(EVENTS.REMOVE_INVOICE, id)
+    this.emit(EVENT.REMOVE_INVOICE, id)
     this.addInvoice(invoice)
-    this.emit(EVENTS.ADD_INVOICE, invoice)
+    this.emit(EVENT.ADD_INVOICE, invoice)
   }
   highlight(id: number) {
     if (id === this.highlightId) {
-      this.emit(EVENTS.HIGHLIGHT_INVOICE, { id, flag: false })
+      this.emit(EVENT.HIGHLIGHT_INVOICE, { id, flag: false })
       return
     }
     if (this.highlightId !== undefined) {
-      this.emit(EVENTS.HIGHLIGHT_INVOICE, { id: this.highlightId, flag: false })
+      this.emit(EVENT.HIGHLIGHT_INVOICE, { id: this.highlightId, flag: false })
     }
-    this.emit(EVENTS.HIGHLIGHT_INVOICE, { id, flag: true })
+    this.emit(EVENT.HIGHLIGHT_INVOICE, { id, flag: true })
     this.highlightId = id
   }
   clear() {
     this.invoices = new Array<Invoice>()
-    this.emit(EVENTS.CLEAR_INVOICES)
+    this.emit(EVENT.CLEAR_INVOICES)
   }
 }
