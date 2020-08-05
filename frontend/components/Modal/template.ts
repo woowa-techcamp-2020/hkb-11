1864a4d6d5d3e9c6159def821c434629bc4c6ffa
Choibumsu <bumsu0211@gmail.com>
2020-08-05 23:34:34 +0900

    [#68] style(modal) - 모달 스타일 기초 작업
    - 폰트(noto sans, framework7 icon) 적용
    - 모달 배경 및 모달 모양 스타일 작업

--- frontend/components/Modal/template.ts
+++ frontend/components/Modal/template.ts
@@ -6,13 +6,13 @@ export const template: string = /*html*/ `
         <div>
           결제 수단 관리
         </div>
-        <div class="float button-close-modal">X</div>
+        <i class="float close-btn f7-icons">xmark</i>
       </div>
       <div class="modal-body">
         <form class="payment-form">
           <label>결제 수단 이름</label>
           <input class="input-payment" />
-          <button class="button button-add-payment">등록</button>
+          <button type="button" class="button button-add-payment">등록</button>
         </form>
         <div class="rows payment-list"></div>
       </div>
@@ -24,7 +24,7 @@ export const paymentTemplate: string = /*html*/ `
   <div class='row'>
     <div class="item center">현대카드</div>
     <div class="item right">
-      <button class="button-remove-payment">X</button>
+      <i class="remove-btn f7-icons">trash</i>
     </div>
   </div>
 `
