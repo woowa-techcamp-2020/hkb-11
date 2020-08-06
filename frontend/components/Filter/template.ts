e7530b813d762423dc6523c107ae733289cf473b
Choibumsu <bumsu0211@gmail.com>
2020-08-06 16:32:35 +0900

    [#94] style(filter) - 필터 스타일 작업

--- frontend/components/Filter/template.ts
+++ frontend/components/Filter/template.ts
@@ -1,14 +1,22 @@
 export const template: string = /*html*/ `
   <section id='invoice-filter'>
-    <div>
-      <input type="checkbox" class="earning-checkbox" />
-      <label>수입</label>
-      <label class="earning-total">3,234,123원</label>
+    <div class='row earning'>
+      <div class='item left'>
+        <input type="checkbox" class="earning-checkbox" />
+        <label>수입</label>
+      </div>
+      <div class='item right'>
+        <label class="earning-total">3,234,123원</label>
+      </div>
     </div>
-    <div>
-      <input type="checkbox" class="spending-checkbox" />
-      <label>지출</label>
-      <label class="spending-total">134,123원</label>
+    <div class='row spending'>
+      <div class='item left'>
+        <input type="checkbox" class="spending-checkbox" />
+        <label>지출</label>
+      </div>
+      <div class='item right'>
+        <label class="spending-total">134,123원</label>
+      </div>
     </div>
   </section>
 `
