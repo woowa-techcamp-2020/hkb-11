d6f2724fc3f7d71a4fa0c3d4b3792bb86224e17f
Choibumsu <bumsu0211@gmail.com>
2020-08-06 17:19:08 +0900

    [#83] style(header) - 헤더 스타일 작업
    - 정렬 변경으로 위한 마크업 수정
    - 모든 버튼, 링크 커서 적용

--- frontend/components/Header/template.ts
+++ frontend/components/Header/template.ts
@@ -1,10 +1,14 @@
 export const template: string = /*html*/ `
   <header id="header">
-    <div class="row">
-      <div class="item center main-title"><a to="list">룰루랄라 가계부</a></div>
-      <a class="link-login" to="login">로그인</a>
-      <a class="link-logout hidden">로그아웃</a>
-      <button class="item right button-payment-modal">결제 수단 관리</button>
+    <div class="row" id="main-header">
+      <div class='item left'>
+        <div class="main-title">룰루랄라 가계부</div>
+      </div>
+      <div class='item right'>
+        <a class="link-login" to="login">로그인</a>
+        <a class="link-logout hidden">로그아웃</a>
+        <button class="button-payment-modal">결제 수단 관리</button>
+      </div>
     </div>
   </header>
 `
