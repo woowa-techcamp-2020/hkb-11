ca386c97bf32096aed2bc9c62bdae713958ba93a
Choibumsu <bumsu0211@gmail.com>
2020-08-07 13:51:43 +0900

    [#106] style(login) - 로그인 페이지 스타일 작업

--- frontend/components/Login/template.ts
+++ frontend/components/Login/template.ts
@@ -2,12 +2,10 @@ export const template: string = /*html*/ `
   <div id="login">
     <form class="rows">
       <div class="row">
-        <label for="id">아이디</label>
-        <input id="id" name="id" class="form">
+        <input id="id" name="id" class="form" placeholder='ID'>
       </div>
       <div class="row">
-        <label for="password">비밀번호</label>
-        <input type="password" id="password" name="password" class="form">
+        <input type="password" id="password" name="password" class="form" placeholder='PASSWORD'>
       </div>
       <div class="row">
         <button type="button" class="button button-login submit">로그인</button>
